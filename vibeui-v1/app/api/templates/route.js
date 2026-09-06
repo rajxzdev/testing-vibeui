790d4f9f4b0137f0665dfdda5781217d275f6902
rajxzdev <rajxz8273@gmail.com>
2026-09-07 02:36:53 +0700

    chore: hapus vibeui-v1/app/api/templates/route.js via GitUp

--- vibeui-v1/app/api/templates/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import templates from '@/lib/templates.json';
-export async function GET() {
-  return NextResponse.json({
-    success: true,
-    templates: templates.map(({ code_html, ...t }) => t),
-  });
-}
