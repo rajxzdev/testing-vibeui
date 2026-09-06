872f4487e2339c5a8a3bad9a83dc0fe92f97c00c
rajxzdev <rajxz8273@gmail.com>
2026-09-07 02:37:38 +0700

    chore: hapus vibeui-v1/next.config.js via GitUp

--- vibeui-v1/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  devIndicators: false,
-  compiler: { removeConsole: process.env.NODE_ENV === 'production' ? { exclude: ['error'] } : false },
-};
